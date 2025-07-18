74c6378132d6c55aba4582b20dd283e6a611c5e4
Muhammad Anas <anasabid607@gmail.com>
2025-07-18 14:58:33 +0200

    Update rev_wstr.c

--- rev_wstr.c
+++ rev_wstr.c
@@ -1,42 +1,44 @@
-int isspace(int c)
-{
-	return (c==' ' || (c>=9 && c<=13));
-}
 #include <unistd.h>
-void print_word(char *str, int start, int end)
+
+int     ft_isspace(int c)
 {
-    while (start < end)
-        write(1, &str[start++], 1);
+        return (c == ' ' || (c >=9 && c <=13));
 }
 
-void rev_wstr(char *str)
+void    print_word(char *str, int start, int end)
 {
-    int len = 0;
-	int end;
-    int start;
-	
-    while (str[len])
-        len++;
-    end = len;
-    start = len - 1;
-    while (start >= 0)
-    {
-        if (isspace(str[start]))
+        while (start < end)
+                write(1, &str[start++], 1);
+}
+
+void    rev_wstr(char *str)
+{
+        int     i;
+        int     end;
+        int     start;
+
+        i = 0;
+        while (str[i])
+                i++;
+        end = i;
+        i--;
+        while (i >= 0)
         {
-            print_word(str, start + 1, end);
-            write(1, " ", 1);
-            end = start;
+                if (ft_isspace(str[i]))
+                {
+                        print_word(str, i + 1, end);
+                        write (1, " ", 1);
+                        end = i;
+                }
+                i--;
         }
-        start--;
-    }
-    if (end > 0)
         print_word(str, 0, end);
 }
 
-int main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-    if (argc == 2)
-        rev_wstr(argv[1]);
-    write(1, "\n", 1);
-    return 0;
+        if (argc == 2)
+                rev_wstr(argv[1]);
+        write(1, "\n", 1);
+        return (0);
 }
